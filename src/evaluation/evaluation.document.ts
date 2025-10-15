d233aeb85a71ff3023e0d08cad2ae752af13f39e
guevaracj8 <guevaracj8@gmail.com>
2025-10-14 22:49:49 -0400

    feat(evaluation): add provider evaluation module with rating logic and Swagger documentation

---
+++ src/evaluation/evaluation.document.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Evaluation {
+  @Prop({ required: true })
+  organizerUserId: string;
+
+  @Prop({ required: true })
+  providerId: string;
+
+  @Prop({ required: true, min: 0, max: 5 })
+  score: number;
+}
+
+export type EvaluationDocument = HydratedDocument<Evaluation>;
+export const EvaluationSchema = SchemaFactory.createForClass(Evaluation);
