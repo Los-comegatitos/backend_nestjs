c144fb79abb166f2c3cc55e4332c8f0c34aba8fb
linopaul_1512 <linobenavidesgabaldon.com>
2025-10-20 06:41:09 -0400

    feat: specialized handler for a provider's tasks

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
+import { TaskProviderController } from './task-provider.controller';
 import { EventModule } from 'src/event/event.module';
 
 @Module({
   imports: [EventModule],
-  controllers: [TaskController],
+  controllers: [TaskController, TaskProviderController],
   providers: [TaskService],
+  exports: [TaskService],
 })
 export class TaskModule {}
