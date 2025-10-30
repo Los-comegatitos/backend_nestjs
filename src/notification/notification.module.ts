882063231e3c76e232389613ae50665a89e30a6f
Luna Hazuki <lunahazuki2006@outlook.com>
2025-10-30 09:12:37 -0400

    feat: with a different expose

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -28,9 +28,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             pass: configService.get<string>('APP_PASSWORD'),
           },
           // requireTLS: true,
-          connectionTimeout: 10000,
-          greetingTimeout: 5000,
-          socketTimeout: 10000,
+          // connectionTimeout: 10000,
+          // greetingTimeout: 5000,
+          // socketTimeout: 10000,
           tls: {
             // ciphers: 'SSLv3',
             rejectUnauthorized: false,
